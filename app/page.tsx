5b143a34601f71ca65d3b8f78a49e592022319af
xueqing <lixueqing07@gmail.com>
2023-12-04 03:39:55 +0800

    add copy did button

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
 "use client";
 
+import { ethers } from "ethers";
+import { randomBytes } from "crypto";
 import { useRef, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { getContractWithSignerInLocalStorage } from "./smartContract";
 import MagnifyingGlass from "./assets/MagnifyingGlass";
 import PageContainer from "./components/PageContainer";
-import ChevronRight from "./assets/ChevronRight";
 import ErrorSadFace from "./assets/ErrorSadFace";
-import { getContractWithSignerInLocalStorage } from "./smartContract";
-import { randomBytes } from "crypto";
-import { ethers } from "ethers";
+import ChevronRight from "./assets/ChevronRight";
+import Copy from "./assets/Copy";
 
 interface tab {
   value: string;
   display: string;
 }
 
-interface Vc {
-  title: string;
-  dateTime: string;
+interface VcButtonProps {
+  vc: VerificationCredentialDocument;
+  onClick: (
+    e: React.MouseEvent<HTMLElement>,
+    vc: VerificationCredentialDocument
+  ) => void;
 }
 
-interface VcButtonProps {
-  vc: Vc;
-  onClick: (e: React.MouseEvent<HTMLElement>, vc: Vc) => void;
+interface VcDetailsProps {
+  vc: VerificationCredentialDocument;
+  showDownloadButton?: boolean;
+  showDeleteButton?: boolean;
+  onClickDownload?: (e: React.MouseEvent<HTMLElement>) => void;
+  onClickDelete?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
 type VerificationCredentialDocument = {
@@ -41,7 +49,8 @@ type VerificationCredentialDocument = {
 
 export default function VerificationCredentials() {
   const [inputFocus, setInputFocus] = useState<boolean>(false);
-  const [selectedVc, setSelectedVc] = useState<Vc | null>(null);
+  const [selectedVc, setSelectedVc] =
+    useState<VerificationCredentialDocument | null>(null);
   const [selectedTab, setSelectedTab] = useState<tab>({
     value: "others",
     display: "Other VCs",
@@ -79,6 +88,7 @@ export default function VerificationCredentials() {
         email: didDocument.service[0].serviceEndpoint,
       },
     };
+    setPersonalVc(vcDocument);
     console.log(vcDocument);
 
     // obtain hash of vcDocument
@@ -106,6 +116,7 @@ export default function VerificationCredentials() {
   };
 
   const [vcs, setVcs] = useState<any>([]);
+  const [personalVc, setPersonalVc] = useState<any>(null);
   const importRef = useRef<any | null>(null);
   const handleImport = (file: any) => {
     setVcs([
@@ -147,33 +158,7 @@ export default function VerificationCredentials() {
                   <p className="text-theme-white">Back</p>
                 </button>
               </div>
-
-              <div className="w-full flex flex-col justify-center items-start p-5 space-y-5 bg-theme-medium-gray rounded-xl">
-                <div className="flex flex-col justify-center items-start">
-                  <p className="text-theme-light-gray text-sm">Issuer</p>
-                  <p className="">{selectedVc.title}</p>
-                </div>
-                <div className="flex flex-col justify-center items-start">
-                  <p className="text-theme-light-gray text-sm">Address</p>
-                  <p className="">asdfghjkl</p>
-                </div>
-                <div className="flex flex-col justify-center items-start">
-                  <p className="text-theme-light-gray text-sm">Date</p>
-                  <p className="">23 November 2023</p>
-                </div>
-                <div className="flex flex-col justify-center items-start">
-                  <p className="text-theme-light-gray text-sm">Time</p>
-                  <p className="">11:53AM</p>
-                </div>
-                <div className="w-full flex flex-row justify-end items-center space-x-4">
-                  <button className="py-3 px-6 rounded-xl text-sm bg-theme-light-gray/20">
-                    <p>Download</p>
-                  </button>
-                  <button className="py-3 px-8 rounded-xl text-sm bg-red-500">
-                    <p>Delete</p>
-                  </button>
-                </div>
-              </div>
+              <VcDetails showDeleteButton showDownloadButton vc={selectedVc} />
             </>
           ) : (
             <>
@@ -205,6 +190,8 @@ export default function VerificationCredentials() {
             </>
           )}
         </div>
+      ) : personalVc ? (
+        <VcDetails showDeleteButton showDownloadButton vc={personalVc} />
       ) : (
         <div className="w-full flex flex-col justify-center items-center p-5 space-y-5 bg-theme-medium-gray rounded-xl">
           <ErrorSadFace className="w-44 h-auto text-theme-white" />
@@ -229,12 +216,89 @@ function VcButton({ vc, onClick }: VcButtonProps) {
     >
       <div className="flex flex-col justify-center items-start">
         <p className="text-theme-light-gray text-sm">Issuer</p>
-        <p className="">{vc.title}</p>
+        <p className="">{vc.issuer}</p>
       </div>
       <div className="flex flex-col justify-center items-start">
         <p className="text-theme-light-gray text-sm">Issued Date</p>
-        <p className="">{vc.dateTime}</p>
+        <p className="">{vc.issuanceDate}</p>
       </div>
     </button>
+  );
+}
+
+function VcDetails({
+  vc,
+  showDownloadButton = false,
+  showDeleteButton = false,
+  onClickDownload,
+  onClickDelete,
+}: VcDetailsProps) {
+  return (
+    <div className="w-full flex flex-col justify-center items-start p-5 space-y-5 bg-theme-medium-gray rounded-xl">
+      {vc.issuer && (
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-theme-light-gray text-sm">DID</p>
+          <div className="flex flex-row space-x-4">
+            <p className="">{vc.issuer}</p>
+            <CopyToClipboard
+              text={vc.issuer}
+              onCopy={() => alert("Copied to clipboard")}
+            >
+              <button>
+                <Copy className="w-4 h-auto text-theme-white" />
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+      )}
+      {vc.issuanceDate && (
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-theme-light-gray text-sm">Issued Date</p>
+          <p className="">{vc.issuanceDate}</p>
+        </div>
+      )}
+      {vc.name && (
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-theme-light-gray text-sm">VC Name</p>
+          <p className="">{vc.name}</p>
+        </div>
+      )}
+      {vc.credentialSubject && vc.credentialSubject.firstName && (
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-theme-light-gray text-sm">First Name</p>
+          <p className="">{vc.credentialSubject.firstName}</p>
+        </div>
+      )}
+      {vc.credentialSubject && vc.credentialSubject.lastName && (
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-theme-light-gray text-sm">Last Name</p>
+          <p className="">{vc.credentialSubject.lastName}</p>
+        </div>
+      )}
+      {vc.credentialSubject && vc.credentialSubject.email && (
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-theme-light-gray text-sm">Email</p>
+          <p className="">{vc.credentialSubject.email}</p>
+        </div>
+      )}
+      <div className="w-full flex flex-row justify-end items-center space-x-4">
+        {showDownloadButton && (
+          <button
+            className="py-3 px-6 rounded-xl text-sm bg-theme-light-gray/20"
+            onClick={(e) => onClickDownload && onClickDownload(e)}
+          >
+            <p>Download</p>
+          </button>
+        )}
+        {showDeleteButton && (
+          <button
+            className="py-3 px-8 rounded-xl text-sm bg-red-500"
+            onClick={(e) => onClickDelete && onClickDelete(e)}
+          >
+            <p>Delete</p>
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
